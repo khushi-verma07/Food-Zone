366da052e13e1b880539d6dc1980bfca1f6a5db7
Khushi Verma <khushivrma07@gmail.com>
2025-08-25 10:38:45 +0530

    Fix admin backend URL to use production environment variable

--- frontend/src/admin-assets/assets.js
+++ frontend/src/admin-assets/assets.js
@@ -1,24 +1,3 @@
-// import logo from './logo.png'
-// import profile_image from './profile_image.png'
-// import upload_area from './upload_area.png'
-// import parcel_icon from './parcel_icon.png'
-// import g_mainlogo from './g_mainlogo.png'
-// import add_icon from './add_icon.gif'
-// import list_icon from './list_icon.gif'
-// import order_icon from './order_icon.gif'
-// export const assets ={
-//     logo,
-//     profile_image,
-//     upload_area,
-//     parcel_icon,
-//     g_mainlogo,
-//     add_icon,
-//     list_icon,
-//     order_icon
-// }
-
-// export const url = 'http://localhost:4000'
-
 import logo from './logo.png'
 import profile_image from './profile_image.png'
 import upload_area from './upload_area.png'
@@ -39,4 +18,4 @@ export const assets = {
   order_icon
 };
 
-export const url = 'http://localhost:4000';
+export const url = import.meta.env.VITE_BACKEND_URL || 'http://localhost:4000';
