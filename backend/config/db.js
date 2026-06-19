28e620f660e405ccaf790df50a42a97dd3be7907
khushi-verma07 <khushivrma07@gmail.com>
2026-06-19 09:34:48 +0530

    commit file db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect(
-      "mongodb+srv://khushivrma07:mongo123@cluster0.jy4w4ik.mongodb.net/food-del"
-    ).then(()=>console.log("DB Connected"));
+    const uri = process.env.MONGODB_URI || "mongodb+srv://khushivrma07:mongo123@cluster0.jy4w4ik.mongodb.net/food-del";
+    await mongoose.connect(uri).then(()=>console.log("DB Connected"));
 };
